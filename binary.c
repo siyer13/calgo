9904b816c1d280b1891de0e50b248064bf8659b4
Sridhar <sreedharriyer@gmail.com>
2018-02-03 16:47:54 -0800

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,1 +1,26 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+void binary(int number)
+{
+  int bin[10];
+  int i = 0;
+
+  for(i=0; number > 0; i++)
+  {
+    bin[i] = number % 2;
+    number = number / 2;
+  }
+  
+  for(i=i-1; i >=0; i--)
+  {
+    printf("%d",bin[i]);
+  }
+
+}
+
+int main()
+{
+  printf("Decimal to Binary\n");
+  binary(256);
+}
